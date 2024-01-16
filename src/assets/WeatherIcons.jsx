952463bb70c8b1e97bdb4a4b38c4c33e07c24f8c
Sameh El Zankalony <zankalony@gmail.com>
2024-01-16 20:45:34 +0200

    finalizing fetch weather functions

---
+++ src/assets/WeatherIcons.jsx
@@ -0,0 +1,22 @@
+const weatherIcons = [
+  {
+    condition: 'clear sky',
+    icon: '<WiDaySunny />',
+  },
+  {
+    condition: 'few clouds',
+    icon: '<WiCloud />',
+  },
+  {
+    condition: 'overcast clouds',
+    icon: '<WiCloudy />',
+  },
+  {
+    condition: 'rain',
+    icon: '<WiDayRain />',
+  },
+];
+
+export default weatherIcons;
+
+
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,28 +1,56 @@
-// import * as dotenv from 'dotenv';
-// dotenv.config();
 import { FaSearchLocation } from 'react-icons/fa';
-import { WiCloud } from 'react-icons/wi';
+import { WiCelsius } from 'react-icons/wi';
 import { useState } from 'react';
-import GetWeatherData from './getWeatherData';
+import weatherIcons from '../assets/WeatherIcons';
 
 const WeatherCard = () => {
-  const [weather, setWeather] = useState('');
+  const [weather, setWeather] = useState([]);
   const [city, setCity] = useState('');
+  const [temp, setTemp] = useState('');
+  const [condition, setCondition] = useState('');
+  const [icon, setIcon] = useState('');
 
   const handleChange = (e) => {
-    e.preventDefault();
+    // e.preventDefault();
     setCity(e.target.value);
   };
 
-  const handleSubmit = async (e) => {
+  const getWeatherData = async (e) => {
     e.preventDefault();
-    const weatherData = await GetWeatherData(city);
-    setWeather(weatherData);
+    const myApi = import.meta.env.VITE_API_KEY;
+    const url = import.meta.env.VITE_API_URL;
+
+    // const response = await fetch(
+    //   `${url}/weather?q=${city}&appid=${myApi}&units=metric`
+    // );
+    // const data = await response.json();
+
+    // if (data) {
+    //   setTemp(Math.round(data.main.temp));
+    //   setCondition(data.weather[0].description);
+    //   return temp, condition;
+    // }
+
+    // response.json().then((weather) => {
+    //   setTemp(Math.round(weather.main.temp));
+    //   setCondition(weather.weather[0].description);
+    // });
+
+    await fetch(`${url}/weather?q=${city}&appid=${myApi}&units=metric`)
+      .then((res) => res.json())
+      .then((data) => {
+        setWeather(data);
+        setTemp(Math.round(data.main.temp));
+        setCondition(data.weather[0].description);
+        setIcon(data.weather[0].icon);
+        console.log(icon);
+      });
   };
+  const src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
 
   return (
     <div className="card">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={getWeatherData}>
         <input
           type="text"
           placeholder="Insert city name..."
@@ -33,12 +61,17 @@ const WeatherCard = () => {
           <FaSearchLocation />
         </button>
       </form>
-      <div>
-        <WiCloud />
+      <div className="weather-icon">
+        <img src={src} alt={condition} />
       </div>
       <div>
-        <p>{weather.temp}</p>
-        <p>{weather.condition}</p>
+        <div className='temp'>
+          <p>{temp ? temp : 'Temp'}</p>{' '}
+          <span className="unit">
+            <WiCelsius />
+          </span>
+        </div>
+        <p>{condition ? condition : 'Condition'}</p>
       </div>
     </div>
   );
