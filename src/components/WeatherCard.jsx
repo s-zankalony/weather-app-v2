8d068b867da728a404accfb4dc1a2a2a950bb25c
Sameh El Zankalony <zankalony@gmail.com>
2024-01-17 22:53:55 +0200

    adding some styling and enhancing code

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,7 +1,6 @@
 import { FaSearchLocation } from 'react-icons/fa';
 import { WiCelsius } from 'react-icons/wi';
 import { useState } from 'react';
-import weatherIcons from '../assets/WeatherIcons';
 
 const WeatherCard = () => {
   const [weather, setWeather] = useState([]);
@@ -9,6 +8,8 @@ const WeatherCard = () => {
   const [temp, setTemp] = useState('');
   const [condition, setCondition] = useState('');
   const [icon, setIcon] = useState('');
+  const [name, setName] = useState('');
+  const [weatherIconSrc, setWeatherIconSRC] = useState('');
 
   const handleChange = (e) => {
     // e.preventDefault();
@@ -36,17 +37,32 @@ const WeatherCard = () => {
     //   setCondition(weather.weather[0].description);
     // });
 
-    await fetch(`${url}/weather?q=${city}&appid=${myApi}&units=metric`)
-      .then((res) => res.json())
-      .then((data) => {
-        setWeather(data);
-        setTemp(Math.round(data.main.temp));
-        setCondition(data.weather[0].description);
-        setIcon(data.weather[0].icon);
-        console.log(icon);
-      });
+    try {
+      await fetch(`${url}/weather?q=${city}&appid=${myApi}&units=metric`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.message) {
+            setName(data.message);
+            setTemp('');
+            setCondition('');
+            setIcon('');
+            setWeatherIconSRC('');
+          } else {
+            setWeather(data);
+            setTemp(Math.round(data.main.temp));
+            setCondition(data.weather[0].description);
+            setIcon(data.weather[0].icon);
+            setName(data.name);
+            setWeatherIconSRC(
+              `https://openweathermap.org/img/wn/${icon}@2x.png`
+            );
+          }
+        });
+    } catch (error) {
+      console.log(error);
+    }
   };
-  const src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+  // const src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
 
   return (
     <div className="card">
@@ -55,17 +71,19 @@ const WeatherCard = () => {
           type="text"
           placeholder="Insert city name..."
           name="city"
+          className="city"
           onChange={handleChange}
         />
-        <button type="submit">
+        <button className="search-btn" type="submit">
           <FaSearchLocation />
         </button>
       </form>
+      <h4>{name}</h4>
       <div className="weather-icon">
-        <img src={src} alt={condition} />
+        <img src={weatherIconSrc ? weatherIconSrc : ''} alt={condition} />
       </div>
       <div>
-        <div className='temp'>
+        <div className="temp">
           <p>{temp ? temp : 'Temp'}</p>{' '}
           <span className="unit">
             <WiCelsius />
