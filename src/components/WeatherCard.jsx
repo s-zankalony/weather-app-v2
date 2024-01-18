576beef18d882eee9a0daab8e288e9390bf1cd11
Sameh El Zankalony <zankalony@gmail.com>
2024-01-19 00:05:15 +0200

    update styling

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -2,6 +2,33 @@ import { FaSearchLocation } from 'react-icons/fa';
 import { WiCelsius } from 'react-icons/wi';
 import { useState } from 'react';
 
+import PropTypes from 'prop-types';
+
+function SearchCard({ getData, handleChange }) {
+  // Component code here
+  return (
+    <div className="card search-card">
+      <form onSubmit={getData}>
+        <input
+          type="text"
+          placeholder="Insert city name..."
+          name="city"
+          className="city-input"
+          onChange={handleChange}
+        />
+        <button className="search-btn" type="submit">
+          <FaSearchLocation />
+        </button>
+      </form>
+    </div>
+  );
+}
+
+SearchCard.propTypes = {
+  getData: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+
 const WeatherCard = () => {
   // const [weather, setWeather] = useState([]);
   const [city, setCity] = useState('');
@@ -10,10 +37,8 @@ const WeatherCard = () => {
   const [icon, setIcon] = useState('');
   const [name, setName] = useState('');
   const [weatherIconSrc, setWeatherIconSRC] = useState('');
-  const weatherImage = '<img src={weatherIconSrc} />';
 
   const handleChange = (e) => {
-    // e.preventDefault();
     setCity(e.target.value);
   };
 
@@ -49,16 +74,19 @@ const WeatherCard = () => {
             setIcon('');
             setWeatherIconSRC('');
             document.getElementById('city-title').style.color = 'red';
+            document.getElementById('result-card').style.display = 'none';
+            document.getElementById('error-card').style.display = 'block';
           } else {
             setIcon(data.weather[0].icon);
             setWeatherIconSRC(
               `https://openweathermap.org/img/wn/${icon}@2x.png`
             );
-            // setWeather(data);
             setTemp(Math.round(data.main.temp));
             setCondition(data.weather[0].description);
             setName(data.name);
             document.getElementById('city-title').style.color = 'inherit';
+            document.getElementById('result-card').style.display = 'block';
+            document.getElementById('error-card').style.display = 'none';
           }
         });
     } catch (error) {
@@ -67,33 +95,27 @@ const WeatherCard = () => {
   };
 
   return (
-    <div className="card">
-      <form onSubmit={getWeatherData}>
-        <input
-          type="text"
-          placeholder="Insert city name..."
-          name="city"
-          className="city"
-          onChange={handleChange}
-        />
-        <button className="search-btn" type="submit">
-          <FaSearchLocation />
-        </button>
-      </form>
-      <h4 id="city-title">{name}</h4>
-      <div className="weather-icon">
-        <img src={weatherIconSrc} />
+    <>
+      <SearchCard handleChange={handleChange} getData={getWeatherData} />
+      <div id="error-card" className="card">
+        <h4 id="city-title">{name}</h4>
       </div>
-      <div>
-        <div className="temp">
-          <p>{temp ? temp : 'Temp'}</p>{' '}
-          <span className="unit">
-            <WiCelsius />
-          </span>
+      <div id="result-card" className="card">
+        <h4 id="city-title">{name}</h4>
+        <div className="weather-icon">
+          <img src={weatherIconSrc} />
         </div>
-        <p className="condition">{condition ? condition : 'Condition'}</p>
+        <div>
+          <div className="temp">
+            <p>{temp ? temp : 'Temp'}</p>{' '}
+            <span className="unit">
+              <WiCelsius />
+            </span>
+          </div>
+          <p className="condition">{condition ? condition : 'Condition'}</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
